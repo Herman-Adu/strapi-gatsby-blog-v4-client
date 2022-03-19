a23017fe1a44e6dd6cb443969fb942fab3502b03
Herman Adu <herman.adu@hotmail.com>
2022-03-19 19:06:14 +0000

    initial upload of gatsby client v4

---
+++ gatsby-config.js
@@ -0,0 +1,71 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+  // eslint-disable-next-line prettier/prettier
+});
+
+module.exports = {
+  plugins: [
+    "gatsby-plugin-gatsby-cloud",
+    "gatsby-plugin-postcss",
+    {
+      resolve: "gatsby-source-strapi",
+      options: {
+        apiURL: process.env.STRAPI_API_URL || "http://localhost:1337",
+        accessToken: process.env.STRAPI_TOKEN,
+        collectionTypes: [
+          {
+            singularName: "article",
+            queryParams: {
+              publicationState: process.env.GATSBY_IS_PREVIEW
+                ? "preview"
+                : "live",
+              populate: {
+                cover: "*",
+                category: {
+                  populate: "*",
+                },
+                blocks: {
+                  populate: "*",
+                },
+              },
+            },
+          },
+          {
+            singularName: "author",
+          },
+          {
+            singularName: "category",
+          },
+        ],
+        singleTypes: [
+          {
+            singularName: "about",
+            queryParams: {
+              populate: {
+                blocks: {
+                  populate: "*",
+                },
+              },
+            },
+          },
+          {
+            singularName: "global",
+            queryParams: {
+              populate: {
+                favicon: "*",
+                defaultSeo: {
+                  populate: "*",
+                },
+              },
+            },
+          },
+        ],
+      },
+    },
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    "gatsby-transformer-remark",
+  ],
+  // eslint-disable-next-line prettier/prettier
+};
